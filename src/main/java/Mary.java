d7accbdd214983a64716257d4bf3f2b0abf72ae9
Chen Xu <chenxu209052@gmail.com>
2025-01-23 21:45:17 +0800

    Implement Level-1

--- src/main/java/Mary.java
+++ src/main/java/Mary.java
@@ -1,8 +1,32 @@
+import java.util.Scanner;
+
 public class Mary {
+
+    public void echo() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String repeat = scanner.nextLine();
+            if (repeat.equals("bye")) {
+                System.out.println("_______________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("_______________________________\n");
+                break;
+            }
+            System.out.println("_______________________________");
+            System.out.println(repeat);
+            System.out.println("_______________________________\n");
+        }
+        scanner.close();
+    }
+
     public static void main(String[] args) {
+        Mary mary = new Mary();
         String welcome = "Hello! I'm Mary \n"
                 + "What can I do for you? \n\n"
-                + "Bye. Hope to see you again soon!";
+                + "----------------------------";
+
         System.out.println(welcome);
+        mary.echo();
     }
 }
