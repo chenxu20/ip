09c1cd5f2d0b459bdde721b6499cb606d72e1f58
Chen Xu <chenxu209052@gmail.com>
2025-01-23 13:39:19 +0800

    Implement Level-0

---
+++ src/main/java/Mary.java
@@ -0,0 +1,8 @@
+public class Mary {
+    public static void main(String[] args) {
+        String welcome = "Hello! I'm Mary \n"
+                + "What can I do for you? \n\n"
+                + "Bye. Hope to see you again soon!";
+        System.out.println(welcome);
+    }
+}
